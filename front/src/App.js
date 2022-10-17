681ef6e1413720677bb3c05b71c926a3d83c7091
Lizeth Vega <vegalizeth4@gmail.com>
2022-10-17 18:56:47 -0500

    clase  14 octubre front

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 import React from 'react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 
 
 function App() {
   return (
     <div className="App">
-        <h1>Tienda de mascotas Vety Shop</h1>
-        <img src="./images/vetyshop.png" alt="Aqui va el logo"></img>
+        
+        <Header/>
+        <center>contenido aqui</center>
+        <center>contenido aqui</center>
+        <center>contenido aqui</center>
+        <center>contenido aqui</center>
+        <center>contenido aqui</center>
+        <center>contenido aqui</center>
+
+        <Footer/>
     </div>
+    
   );
 }
 
---
+++ front/src/components/layout/Footer.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const Footer = () => {
+  return (
+    <Fragment>
+<footer className='py-1'>
+        <p className='text-center mt-1'>
+             esto es un footer falta completar
+        </p>
+        </footer>
+    </Fragment>
+    
+  )
+}
+
+export default Footer
