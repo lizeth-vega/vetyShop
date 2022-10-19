661b8e3321bcd0ffe14acb0d247efd229cb38afe
Lizeth Vega <vegalizeth4@gmail.com>
2022-10-19 14:38:04 -0500

    clase 19 octubre

---
+++ front/src/constants/productConstants.js
@@ -0,0 +1,9 @@
+//ver productos registrados
+//Contsantesn con los diferentes estados que van a estar involucrados al ver los productos
+export const ALL_PRODUCTS_REQUEST ="ALL_PRODUCTS_REQUEST"
+export const ALL_PRODUCTS_SUCCESS = "ALL_PRODUCTS_SUCCESS"
+export const ALL_PRODUCTS_FAIL ="ALL_PRODUCTS_FAIL"
+
+//para limpirar posibles errores cometidos en pantalla
+export const CLEAR_ERRORS ="CLEAR_ERRORS"
+
